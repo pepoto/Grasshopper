a6ed046d3e8fadd80594be07a60edcb2a8382f87
pepoto <pepoto71@gmail.com>
2014-02-12 12:51:00 +0200

    fix button

--- TestGraphics/TestGraphics/Form1.Designer.cs
+++ TestGraphics/TestGraphics/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(506, 12);
+            this.button1.Location = new System.Drawing.Point(517, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(139, 61);
             this.button1.TabIndex = 0;
