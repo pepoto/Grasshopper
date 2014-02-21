672e4b81c8a776b3c42ac9a493a30996febe1b6a
DinkoK <dinko.krastev89@gmail.com>
2014-02-21 22:42:38 +0200

    Matrix

---
+++ Grasshopper/Grasshopper/Game.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Grasshopper
+{
+    
+    class Game
+    {
+        const int MAX_WIDTH = 60;
+        const int MAX_HEIGHT = 23;
+        static char[,] playerField = new char[MAX_HEIGHT, MAX_WIDTH];
+        
+        static ConsoleColor fieldGroundColor = ConsoleColor.Gray;
+        static ConsoleColor fieldLinesColor = ConsoleColor.Green;
+
+        static void DrawPlayerField(char[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void PlayerField(char[,] matrix)
+        {
+            char brick = '#';
+            // CONSOLE SETTINGS
+
+            Console.BackgroundColor = fieldGroundColor;
+            Console.ForegroundColor = fieldLinesColor;
+
+            // FILLING UP matrix[,] ARRAY
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                matrix[i, 0] = brick;
+                matrix[i, matrix.GetLength(1) - 1] = brick;
+            }
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                matrix[0, i] = brick;
+                matrix[matrix.GetLength(0) - 1, i] = brick;
+            }
+
+        }
+
+        static void Main ()
+        {
+            PlayerField(playerField);
+            DrawPlayerField(playerField);
+            
+        }
+
+
+
+    }
+}
---
+++ Grasshopper/Grasshopper/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Grasshopper
+{
+    
+    public struct Position
+    {
+        public int row;
+        public int col;
+        public Position(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+    }
+}
