15716dddbc8e8795e6ca1279cfe33daabe88594a
nina75 <nina150575@gmail.com>
2014-02-22 21:57:13 +0200

    ..

---
+++ Grasshopper/Grasshopper/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grasshopper
+{
+    public abstract class Weapon : StaticObject, IResource
+    {
+        protected Weapon(string imageUrl, Point location)
+            : base(imageUrl, location)
+        {
+        }
+        public int Quantity
+        {
+            get { return this.Power; }
+        }
+
+        public ResourceType Type
+        {
+            get { return ResourceType.Weapon; }
+        }
+
+
+        public int Power { get; set; }
+        public bool IsEmpty
+        {
+            get
+            {
+                return this.Power <= 0;
+            }
+        }
+
+
+    }
+}
