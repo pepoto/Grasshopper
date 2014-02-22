9efd7cec1aab5e631c624894b6763c5eb52c4f51
nina75 <nina150575@gmail.com>
2014-02-22 15:02:25 +0200

    Add moving object again :)

--- Grasshopper/Grasshopper/MovingObject.cs
+++ Grasshopper/Grasshopper/MovingObject.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Grasshopper
 {
-    public abstract class Character : MovingObject
+    public class MovingObject : GameObject
     {
     }
 }
